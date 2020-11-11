7379d57b1b59cd9add233ba89c52132876335eac
Cyril <cymojo>
2020-11-11 16:57:28 +0100

    Import svg eyes to vanilla

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Demonstration of SVG in vue"/>
   </div>
 </template>
 
