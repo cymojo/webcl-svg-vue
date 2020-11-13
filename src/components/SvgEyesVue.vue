c4efe2fdd3249dde2f5e854b7ae0b8d92879b14b
Kerkmann <felix.kerkmann@s-p-m.ch>
2020-11-13 13:27:33 +0100

    test mit einem auge

--- src/components/SvgEyesVue.vue
+++ src/components/SvgEyesVue.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
+  <div>
 
-    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "SvgEyesVue"
-    }
+  export default {
+    name: "SvgEyesVue"
+  }
 </script>
 
 <style scoped>
