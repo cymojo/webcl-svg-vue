071a32ff4569de1955ba83907bb625ffbf49d0a7
Felix Kerkmann <felix.kerkmann@students.fhnw.ch>
2020-11-15 20:51:55 +0100

    added second eye hardcoded

--- src/components/SvgEyesVue.vue
+++ src/components/SvgEyesVue.vue
@@ -26,6 +26,32 @@
         <path d="M0 60 A60,60 0 0,0 120,60 A60,40 0 0,1 0,60 Z" opacity="1" fill="#F4CB76" fill-opacity="1" />
       </g>
     </svg>
+    <svg id="eye2" viewBox="0 0 120 120">
+      <filter id="shadow">
+        <feGaussianBlur in="SourceAlpha" stdDeviation="3" />
+        <feOffset       dx="0" dy="8" />
+        <feColorMatrix  type="matrix"
+                        values="0 0 0 0  0
+                                0 0 0 0  0
+                                0 0 0 0  0
+                                0 0 0 .5 0"/>
+        <feBlend        in="SourceGraphic" mode="normal"/>
+      </filter>
+      <radialGradient id="gradient1" gradientUnits="objectBoundingBox" cx="50%" cy="50%" r="50%">
+        <stop offset= "38%" stop-color="#000000" stop-opacity="1" />
+        <stop offset= "46%" stop-color="#073F80" stop-opacity="1" />
+        <stop offset= "90%" stop-color="#8EC0DD" stop-opacity="1" />
+        <stop offset="100%" stop-color="#2F3A46" stop-opacity="1" />
+      </radialGradient>
+      <g id="eye2_iris">
+        <ellipse cx="60" cy="60" rx="30" ry="30" opacity="1" fill="url(#gradient1)" />
+        <ellipse cx="50" cy="50" rx="7"  ry="7"  opacity="1" fill="#FFFFFF" fill-opacity="0.8"/>
+      </g>
+      <g id="eye2_openLids">
+        <path d="M0 60 A60,60 0 0,1 120,60 A60,30 0 0,0 0,60 Z" opacity="1" fill="#FDDC99" fill-opacity="1" filter="url(#shadow)" />
+        <path d="M0 60 A60,60 0 0,0 120,60 A60,40 0 0,1 0,60 Z" opacity="1" fill="#F4CB76" fill-opacity="1" />
+      </g>
+    </svg>
   </div>
 </template>
 
@@ -48,11 +74,25 @@
         scaledX:0,
         xe:0,
         scaledY:0,
-        ye:0
+        ye:0,
+        x2:130,
+        y2:470,
+        width2:40,
+        height2:40,
+        xo2:0,
+        yo2:0,
+        xm2:0,
+        ym2:0,
+        scaledX2:0,
+        xe2:0,
+        scaledY2:0,
+        ye2:0
       }
     },
     methods: {
       follow(evt) {
+
+        /* Eye one */
         this.xo = this.x + this.width/2;
         this.yo = this.y + this.height/2;
         this.xmax = this.width/1.5;
@@ -89,6 +129,35 @@
         document.getElementById("eye_iris").style.visibility = "visible";
         document.getElementById("eye_iris").style.transform = `translateX(${this.xe}px) translateY(${this.ye}px)`;
         //this.iris.style.transform = `translateX(${this.xe}px) translateY(${this.ye}px)`;
+
+
+        /* Eye 2 */
+        this.xo2 = this.x2 + this.width2/2;
+        this.yo2 = this.y2 + this.height2/2;
+        this.xmax = this.width2/1.5;
+        this.ymax= this.height2/2;
+
+        this.xm2 = evt.clientX - this.xo2;
+        this.ym2 = evt.clientY - this.yo2;
+
+        this.scaledX2 = this.xm2 * (this.xmax / this.widestFocus);
+
+        this.xe2 = this.xm2 > 0
+                ? Math.min( this.xmax, this.scaledX2)
+                : Math.max(-this.xmax, this.scaledX2);
+        this.scaledY2 = this.ym2 * (this.ymax / this.widestFocus );
+
+        this.ye2 = this.ym2 > 0
+                ? Math.min( this.ymax, this.scaledY2)
+                : Math.max(-this.ymax, this.scaledY2);
+        if (this.xe2*this.xe2 + this.ye2*this.ye2 > this.xmax * this.ymax) {
+          this.xe2 *= 0.9;
+          this.ye2 *= 0.9;
+        }
+
+        document.getElementById("eye2_iris").style.visibility = "visible";
+        document.getElementById("eye2_iris").style.transform = `translateX(${this.xe2}px) translateY(${this.ye2}px)`;
+        //this.iris.style.transform = `translateX(${this.xe}px) translateY(${this.ye}px)`;
       }
     }
   }
@@ -96,11 +165,18 @@
 </script>
 
 <style scoped>
-  svg {
+  #eye {
     width:  100px;
     height: 80px;
     position: absolute;
     left: 100px;
+    top: 450px;
+  }
+  #eye2 {
+    width:  100px;
+    height: 80px;
+    position: absolute;
+    left: 200px;
     top: 450px;
   }
   #all-area {
@@ -111,6 +187,9 @@
     width: 100%;
   }
   #eye_iris {
+    visibility: hidden;
+  }
+  #eye2_iris {
     visibility: hidden;
   }
 </style>
